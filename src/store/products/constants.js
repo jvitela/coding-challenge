dd1bb4ee86e67c3cc873451e70e129cbaf009566
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-08-06 18:28:33 +0200

    Added constants for Products store
    Including action types for async functions and state machine status.

---
+++ src/store/products/constants.js
@@ -0,0 +1,14 @@
+export const PRODUCTS = "Products";
+export const FETCH_BRANDS = `${PRODUCTS}/fetchBrands`;
+export const FETCH_PRODUCT = `${PRODUCTS}/fetchProduct`;
+export const FETCH_BRAND_PRODUCTS = `${PRODUCTS}/fetchBrandProducts`;
+export const STATUS = {
+  INITIAL: "initial",
+  BRANDS_LIST_LOADING: "brandsList/loading",
+  BRANDS_LIST_READY: "brandsList/ready",
+  PRODUCT_LOADING: "product/loading",
+  PRODUCT_READY: "product/ready",
+  BRAND_LOADING: "brand/loading",
+  BRAND_READY: "brand/ready",
+  ERROR: "error",
+};
