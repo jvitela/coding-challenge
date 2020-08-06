aa5d04350ef69a7138f6c12af0c2b005f2ea5151
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-08-06 18:32:36 +0200

    Added methods to fetch data from the makeup api

--- src/api/makeUp.js
+++ src/api/makeUp.js
@@ -1,24 +1,41 @@
 import ky from "ky";
+import brandNames from "api/brandNames";
 
 const httpClient = ky.extend({
   prefixUrl: "https://makeup-api.herokuapp.com/api/v1",
 });
 
+/**
+ * Retrieves the data for a single product by Id
+ * TODO: Validate input
+ * @param {string} pid
+ */
 function getProduct(pid) {
-  return httpClient.get(`products/${id}.json`).json();
+  return httpClient.get(`products/${pid}.json`).json();
 }
 
-function getProductsList() {
-  return httpClient
-    .get(`products.json`, {
-      searchParams: {
-        brand: "maybelline",
-      },
-    })
-    .json();
+/**
+ * Retrieves a list of products with the given filters
+ * TODO: Validate input
+ * @param {object} searchParams
+ */
+function getAllProducts(searchParams) {
+  return httpClient.get(`products.json`, { searchParams }).json();
+}
+
+/**
+ * Returns brands info
+ */
+function getAllBrands() {
+  return brandNames.map((name, idx) => ({
+    key: idx,
+    text: name,
+    value: name,
+  }));
 }
 
 export default {
   getProduct,
-  getProductsList,
+  getAllProducts,
+  getAllBrands,
 };
