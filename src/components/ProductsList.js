1ee1cb0e89ee9b63407823e107de510ed1ae709d
jvitela <jonathanvitela@Jonathans-MacBook-Pro.local>
2020-08-11 20:42:14 +0200

    Display message on empty results
    If the filter does not return any products, a text stating "No products to be displayed" should appear

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { List } from "semantic-ui-react";
+import { List, Message } from "semantic-ui-react";
 import ProductItem from "components/ProductItem";
 import { useSelector } from "react-redux";
 import { selectProducts } from "store/products/selectors";
@@ -7,10 +7,21 @@ import { selectProducts } from "store/products/selectors";
 export default function ProductsList() {
   const productsList = useSelector(selectProducts);
   return (
-    <List selection divided celled verticalAlign="middle">
-      {productsList.map((item) => (
-        <ProductItem {...item} key={item.id} />
-      ))}
-    </List>
+    <>
+      <List selection divided celled verticalAlign="middle">
+        {productsList.map((item) => (
+          <ProductItem {...item} key={item.id} />
+        ))}
+      </List>
+      {productsList.length === 0 && <EmptyFilterMessage />}
+    </>
+  );
+}
+
+function EmptyFilterMessage() {
+  return (
+    <Message>
+      <Message.Header>No products to be displayed</Message.Header>
+    </Message>
   );
 }
